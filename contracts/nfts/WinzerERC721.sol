c615a83d5e93f54988af470687e2f35589ead9c7
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2022-05-07 15:00:29 +0300

    feat(winzer): add new winzer traits

--- contracts/nfts/WinzerERC721.sol
+++ contracts/nfts/WinzerERC721.sol
@@ -11,27 +11,39 @@ contract WinzerERC721 is ERC721Tradable {
     struct DNA {
         uint8 race;
         uint8 sex;
-        // special skill id
-        uint8 skill;
+        uint8 skin;
+        uint8 head;
+        uint8 ears;
         uint8 hair;
         uint8 beard;
-        // skin color
-        uint8 skin;
-        // face details (scars, face paint, frecles)
-        uint8 face;
-        uint8 eyes;
         uint8 mouth;
+        uint8 eyes;
+        uint8 eyebrows;
+        uint8 nose;
+        uint8 scars;
+    }
+
+    struct ExtraDNA {
+        uint8 accessory;
+        uint8 makeup;
+        uint8 skill1;
+        uint8 skill2;
+        uint8 skill3;
+        uint8 skill4;
+        uint8 skill5;
     }
 
     event DnaUpdated(uint256 indexed _tokenId, DNA _dna);
+    event ExtraDnaUpdated(uint256 indexed _tokenId, ExtraDNA _dna);
 
     mapping(uint256 => DNA) public dna;
+    mapping(uint256 => ExtraDNA) public extraDna;
 
     string private __baseURI = "https://www.winzerland.com/meta/character/";
 
     constructor() ERC721Tradable("Winzerland: Winzer", "Winzer") {}
 
-    function mint(address player, DNA memory _dna)
+    function mint(address player, DNA memory _dna, ExtraDNA memory _extraDna)
         public onlyRole(MINTER_ROLE)
         returns (uint256) 
     {
@@ -39,6 +51,7 @@ contract WinzerERC721 is ERC721Tradable {
         _mint(player, newItemId);
 
         _setDna(newItemId, _dna);
+        _setExtraDna(newItemId, _extraDna);
 
         return newItemId;
     }
@@ -46,6 +59,11 @@ contract WinzerERC721 is ERC721Tradable {
     function _setDna(uint256 _tokenId, DNA memory _dna) internal {
         dna[_tokenId] = _dna;
         emit DnaUpdated(_tokenId, _dna);
+    }
+
+    function _setExtraDna(uint256 _tokenId, ExtraDNA memory _dna) internal {
+        extraDna[_tokenId] = _dna;
+        emit ExtraDnaUpdated(_tokenId, _dna);
     }
 
     function _baseURI() internal view override returns (string memory) {
--- scripts/characters/mint.ts
+++ scripts/characters/mint.ts
@@ -4,10 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers, getNamedAccounts } from "hardhat";
-
-const random = (min: number = 0, max: number = 256) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+import { randomizeWinzerLogically } from '@winzeland/winzer/dist';
 
 async function main() {
   const { deployer } = await getNamedAccounts();
@@ -15,19 +12,34 @@ async function main() {
   // @ts-ignore
   const characters = await ethers.getContract("WinzerERC721");
 
-  const sex = random(0, 1);
+  const traits = randomizeWinzerLogically();
 
-  const tx = await characters.mint(deployer, {
-    race: 0,
-    sex,
-    skill: random(0, 12),
-    hair: random(0, 12),
-    beard: sex === 0 ? random(0, 12) : 0,
-    skin: random(0, 5),
-    face: random(0, 8),
-    eyes: random(0, 24),
-    mouth: random(0, 24),
-  });
+  const dna = {
+    race: traits.race,
+    sex: traits.sex,
+    skin: traits.skinTone,
+    head: traits.head,
+    ears: traits.ears,
+    hair: traits.hair,
+    beard: traits.beard,
+    mouth: traits.mouth,
+    eyes: traits.eyes,
+    eyebrows: traits.eyebrows,
+    nose: traits.nose,
+    scars: traits.scars,
+  };
+
+  const extra = {
+    accessory: traits.accessories,
+    makeup: traits.makeup,
+    skill1: 0,
+    skill2: 0,
+    skill3: 0,
+    skill4: 0,
+    skill5: 0,
+  };
+
+  const tx = await characters.mint(deployer, dna, extra);
 
   await tx.wait();
 }
